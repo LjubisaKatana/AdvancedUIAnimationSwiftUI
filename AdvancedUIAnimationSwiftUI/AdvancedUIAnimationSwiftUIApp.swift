19579f90fd9f09e3db312f395e9902ee8f26e65d
LjubisaKatana <lj.katana85@gmail.com>
2025-10-07 10:51:33 +0200

    Initial Commit

---
+++ AdvancedUIAnimationSwiftUI/AdvancedUIAnimationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedUIAnimationSwiftUIApp.swift
+//  AdvancedUIAnimationSwiftUI
+//
+//  Created by Ljubisa Katana on 7.10.25..
+//
+
+import SwiftUI
+
+@main
+struct AdvancedUIAnimationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
